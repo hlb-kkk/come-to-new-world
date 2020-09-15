7a5aff5d82fb518650b01f9b36eeadfce2755f6c
hlb-kkk <57029888+hlb-kkk@users.noreply.github.com>
2020-09-15 22:18:20 +0800

    code
    essential coed

---
+++ hello_world.cpp
@@ -0,0 +1,29 @@
+#include <iostream> //头文件
+
+using namespace std;
+
+int main()  //主函数
+{
+
+	cout << "hello world" << endl; //代码含义就是在屏幕中输出hello world， endl用于换行
+
+	int m = 5;
+	cout << "m = " << m << endl;
+
+	system("pause"); //使得系统停留于当前运行页面
+
+
+	return 0; // 可以用于判断是否完全运行
+}
+/*    
+
+
+这边
+可以
+进行
+多行
+注释
+
+
+
+*/
